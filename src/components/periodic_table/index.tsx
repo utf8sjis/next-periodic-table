bc9a92156928af23704f693d377d6411c6ab2e5d
utf8sjis <89941856+utf8sjis@users.noreply.github.com>
2025-06-08 20:33:58 +0900

    Add Prettier setup and format code files

--- src/components/periodic_table/index.tsx
+++ src/components/periodic_table/index.tsx
@@ -5,7 +5,7 @@ import styles from "./styles.module.css";
 export default function PeriodicTable() {
   return (
     <div className={styles.base}>
-      {elementsData.map((element) => (
+      {elementsData.map(element => (
         <ElementCell key={element.number} element={element} />
       ))}
     </div>
--- src/utils/elements_data.ts
+++ src/utils/elements_data.ts
@@ -2,7 +2,7 @@ import { elementsData } from "@/constants/elements_data";
 import type { ElementData } from "@/constants/elements_data";
 
 export function getElementDataBySymbol(symbol: string): ElementData {
-  const element = elementsData.find((element) => element.symbol === symbol);
+  const element = elementsData.find(element => element.symbol === symbol);
   if (!element) {
     throw new Error(`Element data with symbol ${symbol} not found`);
   }
